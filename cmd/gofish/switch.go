959a47e19a51f23e02015da155fca5574de305a6
HD Stich <hd@monkeyguru.dev>
2022-03-13 09:38:57 +0100

    Implemented 'switch' command

--- cmd/gofish/switch.go
+++ cmd/gofish/switch.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"errors"
+	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
 )
@@ -12,7 +13,36 @@ func newSwitchCmd() *cobra.Command {
 		Short: "switch fish food to another version",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return errors.New("`gofish switch` is not implemented")
+			f, err := getFood(args[0])
+			if err != nil {
+				return err
+			}
+
+			// oldVersion := f.Version
+
+			pkg := f.GetPackage(runtime.GOOS, runtime.GOARCH)
+			if pkg == nil {
+				return fmt.Errorf("food '%s' does not support the current platform (%s/%s)", f.Name, runtime.GOOS, runtime.GOARCH)
+			}
+
+			if err := f.Unlink(pkg); err != nil {
+				return err
+			}
+
+			versions := findFoodVersions(args[0])
+			if len(versions) >= 1 {
+				for _, version := range versions {
+					if version == args[1] {
+						f.Version = version
+
+						return f.Link(pkg)
+					}
+				}
+
+				return fmt.Errorf("version '%s' of food '%s' not available", args[1], args[0])
+			}
+
+			return nil
 		},
 	}
 	return cmd
